0d7b1010878c5a3b8acf19e31f921261bccdff54
CrisFirey <cristofer.carrasco@uniat.edu.mx>
2025-10-22 11:51:34 -0600

    Player_V1

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Parameters
+    [Header("Motion")]
+    public float speed = 5f;
+    public float gravity = -9.81f;
+
+    [Header("Camera")]
+    public float mouseSensitivity = 2f;
+    public float verticalClamp = 30f;
+    public Transform cameraPlayer;
+
+    #endregion
+}
